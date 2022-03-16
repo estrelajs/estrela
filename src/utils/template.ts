ee7fe5c2ea04d4012d548d5791c75cdc627596a7
Rosostolato <eduardo.rosostolato@gmail.com>
2022-03-16 15:12:45 -0300

    fix(core): fixed attr in tag checker bug

--- src/utils/template.ts
+++ src/utils/template.ts
@@ -7,7 +7,7 @@ export function isHtmlResult(x: any): x is HTMLResult {
 
 /** Checks if current html string is inside a tag. */
 export function isInTag(html: string): boolean {
-  for (let i = html.length; i > 0; --i) {
+  for (let i = html.length - 1; i >= 0; i--) {
     if (html[i] === '<') return true;
     if (html[i] === '>') return false;
   }
