39312b70b2c550a3b8f3f0cfcde4768df6626ea1
Rosostolato <eduardo.rosostolato@gmail.com>
2023-06-27 22:09:22 -0300

    feat(store): added signal store package

--- packages/estrela/src/router/index.ts
+++ packages/estrela/src/router/index.ts
@@ -1,3 +1,12 @@
 export * from './components/Link';
 export * from './components/Router';
 export * from './route';
+export {
+  type NavigateOptions,
+  fragment,
+  href,
+  navigateTo,
+  queryParams,
+  state,
+  url,
+} from './router.store';
---
+++ packages/estrela/src/store/store.ts
@@ -0,0 +1,43 @@
+import { ReadonlySignal, computed, signal } from '../signal';
+import { SignalStoreFeature } from './feature';
+
+type UnionToIntersection<U> = (
+  U extends unknown ? (k: U) => void : never
+) extends (k: infer I) => void
+  ? I
+  : never;
+
+export type SignalStoreState<Args extends SignalStoreFeature[]> =
+  UnionToIntersection<
+    Args[number] extends SignalStoreFeature ? Args[number]['state'] : never
+  >;
+
+export type SignalStore<T> = {
+  [K in keyof T]: ReadonlySignal<T[K]>;
+};
+
+/** Creates a Signal Store. */
+export function signalStore<
+  Args extends SignalStoreFeature[],
+  State extends SignalStoreState<Args>
+>(...args: Args): [SignalStore<State>, (fn: (value: State) => State) => void] {
+  const initialState = {} as State;
+  const storeSignal = signal({} as State);
+  const store = {} as SignalStore<State>;
+  const update = (fn: (value: State) => State) => {
+    storeSignal.update(fn);
+  };
+
+  for (const feature of args) {
+    if (feature.state) {
+      Object.assign(initialState as State & {}, feature.state);
+    }
+  }
+
+  update(() => initialState);
+  for (const key in initialState) {
+    store[key] = computed(() => storeSignal()[key]);
+  }
+
+  return [store, update];
+}
